e724dc51af02bf7a99036070540e6dc9ab3859e3
LoÏs NGA <tonemail@gmail.com>
2026-04-29 02:31:39 +0200

    Fix Google OAuth callback URL

--- config/passport.js
+++ config/passport.js
@@ -2,30 +2,41 @@ const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const User = require("../models/User");
 
+const SERVER_URL =
+  process.env.SERVER_URL ||
+  process.env.BACKEND_URL ||
+  "http://localhost:5000";
+
 passport.use(
   new GoogleStrategy(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/auth/google/callback",
+      callbackURL: `${SERVER_URL}/auth/google/callback`,
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
-        const email = profile.emails[0].value;
+        const email = profile.emails?.[0]?.value;
+        const name = profile.displayName || email;
+
+        if (!email) {
+          return done(null, false);
+        }
 
         let user = await User.findOne({ email });
 
         if (!user) {
           user = await User.create({
-            name: profile.displayName,
+            name,
             email,
             password: "GOOGLE_AUTH_USER",
+            role: "user",
           });
         }
 
-        done(null, user);
+        return done(null, user);
       } catch (err) {
-        done(err, null);
+        return done(err, null);
       }
     }
   )
